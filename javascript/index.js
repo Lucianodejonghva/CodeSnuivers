1689eb4a4b9f416044920bb79964b510bc5708c8
Prolite2003 <Stefan-5@live.nl>
2022-09-14 13:10:10 +0200

    added all the names from our tribe

--- javascript/index.js
+++ javascript/index.js
@@ -6,22 +6,61 @@ const namesFromDOM = document.getElementsByClassName("name");
 
 // listen for user events
 searchInput.addEventListener("keyup", (event) => {
-  const { value } = event.target;
+    const { value } = event.target;
 
-  // get user search input converted to lowercase
-  const searchQuery = value.toLowerCase();
+    // get user search input converted to lowercase
+    const searchQuery = value.toLowerCase();
 
-  for (const nameElement of namesFromDOM) {
-    // store name text and convert to lowercase
-    let name = nameElement.textContent.toLowerCase();
+    for (const nameElement of namesFromDOM) {
+        // store name text and convert to lowercase
+        let name = nameElement.textContent.toLowerCase();
 
-    // compare current name to search input
-    if (name.includes(searchQuery)) {
-      // found name matching search, display it
-      nameElement.style.display = "block";
-    } else {
-      // no match, don't display name
-      nameElement.style.display = "none";
+        // compare current name to search input
+        if (name.includes(searchQuery)) {
+            // found name matching search, display it
+            nameElement.style.display = "block";
+        } else {
+            // no match, don't display name
+            nameElement.style.display = "none";
+        }
     }
-  }
 });
+
+
+var ListOfNames = [
+    { Name: "Stefan" },
+    { Name: "Tobias" },
+    { Name: "Ivar" },
+    { Name: "Suus Ten V." },
+    { Name: "Krijn" },
+    { Name: "Luciano" },
+    { Name: "Maaike" },
+    { Name: "Nazneen" },
+    { Name: "Rayan" },
+    { Name: "Rosa" },
+    { Name: "Sascha" },
+    { Name: "Suus H." },
+    { Name: "Gijs" },
+    { Name: "Stefan" },
+    { Name: "Duneya" },
+    { Name: "Rosa" },
+    { Name: "Demi" },
+    { Name: "Ralph" },
+    { Name: "Youssra" },
+    { Name: "Marwu" },
+    { Name: "Tobias" }
+
+];
+
+let ULID = document.getElementById('laptopsearchresult');
+
+for (var i = 0; i < ListOfNames.length; i++) {
+
+
+    let LiItems = document.createElement('li');
+    LiItems.setAttribute("class", "name");
+    LiItems.innerHTML = ListOfNames[i].Name;
+
+    ULID.appendChild(LiItems);
+
+};
