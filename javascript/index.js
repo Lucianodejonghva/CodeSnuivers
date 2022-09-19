c299faa1a61157254ce4ce0a6463154428ca07a1
Prolite2003 <Stefan-5@live.nl>
2022-09-19 09:25:32 +0200

    Added the typing function the correct way

--- javascript/index.js
+++ javascript/index.js
@@ -4,54 +4,56 @@ var ListOfNames = [
     { Device: "phone" },
     { Name: "Stefan" },
     { Name: "Tobias" },
+    { Name: "Luciano" },
     { Name: "Ivar" },
     { Name: "Suus Ten V." },
     { Name: "Krijn" },
-    { Name: "Luciano" },
     { Name: "Maaike" },
     { Name: "Nazneen" },
-    { Name: "Rayan" },
     { Name: "Rosa" },
     { Name: "Sascha" },
     { Name: "Suus H." },
-    { Name: "Gijs" },
+    { Name: "Rayan" },
     { Name: "Duneya" },
     { Name: "Rosa" },
     { Name: "Demi" },
     { Name: "Ralph" },
     { Name: "Youssra" },
-    { Name: "Marwu" }
-    
+    { Name: "Marwu" },
+    { Name: "Gijs" }
+
 ];
+
+
 for (var i = 0; i < 3; i++) {
-// get search bar element
+    // get search bar element
 
-const searchInput = document.getElementById("studentsearchbar" + ListOfNames[i].Device);
+    const searchInput = document.getElementById("studentsearchbar" + ListOfNames[i].Device);
 
-// store name elements in array-like object
-const namesFromDOM = document.getElementsByClassName("name");
+    // store name elements in array-like object
+    const namesFromDOM = document.getElementsByClassName("name");
 
-// listen for user events
-searchInput.addEventListener("keyup", (event) => {
-    const { value } = event.target;
+    // listen for user events
+    searchInput.addEventListener("keyup", (event) => {
+        const { value } = event.target;
 
-    // get user search input converted to lowercase
-    const searchQuery = value.toLowerCase();
+        // get user search input converted to lowercase
+        const searchQuery = value.toLowerCase();
 
-    for (const nameElement of namesFromDOM) {
-        // store name text and convert to lowercase
-        let name = nameElement.textContent.toLowerCase();
+        for (const nameElement of namesFromDOM) {
+            // store name text and convert to lowercase
+            let name = nameElement.textContent.toLowerCase();
 
-        // compare current name to search input
-        if (name.includes(searchQuery)) {
-            // found name matching search, display it
-            nameElement.style.display = "block";
-        } else {
-            // no match, don't display name
-            nameElement.style.display = "none";
+            // compare current name to search input
+            if (name.includes(searchQuery)) {
+                // found name matching search, display it
+                nameElement.style.display = "block";
+            } else {
+                // no match, don't display name
+                nameElement.style.display = "none";
+            }
         }
-    }
-});
+    });
 
 };
 
@@ -62,10 +64,14 @@ for (var i = 3; i < ListOfNames.length; i++) {
 
     let LiItemsLaptop = document.createElement('li');
     LiItemsLaptop.setAttribute("class", "name");
-    LiItemsLaptop.innerHTML = ListOfNames[i].Name;
 
+    let ATagLaptop = document.createElement('a');
+    ATagLaptop.setAttribute("href", "https://" + ListOfNames[i].Name + ".student.fdnd.nl")
+    ATagLaptop.setAttribute("target", "_blank");
+    ATagLaptop.innerHTML = ListOfNames[i].Name;
 
     ULIDLaptop.appendChild(LiItemsLaptop);
+    LiItemsLaptop.appendChild(ATagLaptop);
 };
 
 
@@ -74,11 +80,18 @@ for (var i = 3; i < ListOfNames.length; i++) {
 let ULIDTablet = document.getElementById('tabletsearchresult');
 
 for (var i = 3; i < ListOfNames.length; i++) {
+
     let LiItemsTablet = document.createElement('li');
     LiItemsTablet.setAttribute("class", "name");
-    LiItemsTablet.innerHTML = ListOfNames[i].Name;
+
+    let ATagTablet = document.createElement('a');
+    ATagTablet.setAttribute("href", "https://" + ListOfNames[i].Name + ".student.fdnd.nl")
+    ATagTablet.setAttribute("target", "_blank");
+    ATagTablet.innerHTML = ListOfNames[i].Name;
+
 
     ULIDTablet.appendChild(LiItemsTablet);
+    LiItemsTablet.appendChild(ATagTablet);
 
 }
 
@@ -88,11 +101,17 @@ for (var i = 3; i < ListOfNames.length; i++) {
 let ULIDPhone = document.getElementById('phonesearchresult');
 
 for (var i = 3; i < ListOfNames.length; i++) {
+
     let LiItemsPhone = document.createElement('li');
     LiItemsPhone.setAttribute("class", "name");
-    LiItemsPhone.innerHTML = ListOfNames[i].Name;
+
+    let ATagPhone = document.createElement('a');
+    ATagPhone.setAttribute("href", "https://" + ListOfNames[i].Name + ".student.fdnd.nl")
+    ATagPhone.setAttribute("target", "_blank");
+    ATagPhone.innerHTML = ListOfNames[i].Name;
+
 
     ULIDPhone.appendChild(LiItemsPhone);
-
+    LiItemsPhone.appendChild(ATagPhone);
 
 }
